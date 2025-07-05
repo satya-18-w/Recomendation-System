c82057e9f3df8c0db15d9df6b8cc9b5e784afe24
satya-18-w <satyajitsamal198076@gmail.com>
2025-07-05 20:39:26 +0530

    first commit starting of the project

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='a hybrid recomendation system that takes care about the user's preference',
+    author='Satyajit samal',
+    license='',
+)
